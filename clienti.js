d53f48e497d4c9e38962c77876b20abc8e0b7f5a
G&L <mairaluigi@hotmail.it>
2026-01-29 09:48:04 +0100

    Update clienti.js

--- clienti.js
+++ clienti.js
@@ -3,19 +3,5 @@ const users = {
     password: "Simona2025@",
     name: "Simona",
     mega: "https://mega.nz/folder/kAUGWSCI#kP_skRX7BFZfPzcrhyFywQ"
-  },
-  "luigimairafotografo@libero.it": {
-    password: "Banzai2025",
-    name: "Luigi Maira",
-    mega: "https://mega.nz/folder/vI42HCCT#BuqP2Sa7oiK67OZKpwlXTQ"
-  },
-"jennyficarra.danza@libero.it": {
-    password: "JennyErika@",
-    name: "Jennifer",
-    mega: "https://mega.nz/folder/https://mega.nz/folder/bJlgHSrK#NDkOX2150XyNFqrxbfTahg"
- },
-  "angelofutsal5@libero.it": {
-    password: "under17",
-    name: "Angelo",
-    mega: "https://mega.nz/folder/https://mega.nz/folder/WjhQBJwQ#hi2nvHSnpUk6OxDJdeAJqA"
+  }
 };
